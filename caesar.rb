afe2d64d5f6442ea9ad2060fae89c7e932a1aa1c
metristo <64803559+metristo@users.noreply.github.com>
2020-06-12 13:42:30 -0400

    Add files via upload

---
+++ caesar.rb
@@ -0,0 +1,38 @@
+
+def cyph(phrase, shift)
+    phrase.map! do |ltr|
+        if ltr.between?(65, 90)
+             if (ltr + shift) >= 90
+                (ltr + shift) - 26
+            else ltr + shift
+            end
+        elsif ltr.between?(97, 122)
+            if (ltr + shift) >= 122
+                (ltr + shift) - 26
+            else (ltr + shift)
+            end
+        else
+            ltr = ltr
+        end
+    end
+
+end
+
+
+puts "Enter a phrase to be encrypted."
+phrase = gets.strip.to_s.bytes
+
+puts "And what would you like your encryption key to be? (just pick a number)"
+shift = gets.strip.to_i
+
+cyph(phrase, shift)
+
+
+
+
+
+
+
+
+    
+    
